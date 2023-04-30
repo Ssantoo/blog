e68b334a9257b251f971c7cb0bc0eba896a0556b
DESKTOP-RM1C3C1\82107 <nowing0108@kakao.com>
2023-04-30 23:26:27 +0900

    라우트

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -5,26 +5,30 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\AuthController;
 
+Route::post('register', []);
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::post('login', [AuthController::class, 'login']);
+//회원가입
 Route::post('register', [AuthController::class, 'register']);
+//로그인
+Route::post('login', [AuthController::class, 'login']);
+//토큰
+Route::get('refresh', [AuthController::class, 'refreshToken']);
 
-Route::middleware(['jwt.verify'])->group(function (){
-    Route::apiResource('posts', PostControllere::class);
-    Route::post('logout', [AuthController::class, 'logout']);
+
+Route::middleware(['jwt.verify'])->group(function () {
+    // 로그아웃
+    Route::get('logout', [AuthController::class, 'logout']);
+    
+    // 게시물 CRUD
+    //글쓰기
+    Route::post('post', [AuthController::class, 'store']);
+    //글내용보기
+    Route::get('post/{id}', [AuthController::class, 'show']);
+    //글수정
+    Route::patch('post/{id}', [AuthController::class, 'update']);
+    //글삭제
+    Route::delete('post/{id}', [AuthController::class, 'destroy']);
+
+    // 특정 사용자 게시물 목록 조회 및 페이징
+    Route::get('board/{userId}', [AuthController::class, 'listByUser']);
 });
