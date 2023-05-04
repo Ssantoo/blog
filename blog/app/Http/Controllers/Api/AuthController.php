171ba6ffed3b03afc531907007e7adf961a13471
DESKTOP-RM1C3C1\82107 <nowing0108@kakao.com>
2023-05-04 23:50:05 +0900

    로그인 수정

--- blog/app/Http/Controllers/Api/AuthController.php
+++ blog/app/Http/Controllers/Api/AuthController.php
@@ -16,18 +16,28 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->validate([
-            'userId' => 'required|stirng',
+            'userId' => 'required|string',
             'password' => 'required|string',
         ]);
-
-        try{
-            if( !$token = JWTAuth::attempt($credentials)){
+    
+        try {
+            if (!$token = JWTAuth::attempt($credentials)){
                 return response()->json(['error' => '접근할 수 없습니다.'], 401);
             }
-        } catch (JWTException $e) {
-            return response()->json(['error' => '토큰 생성 불가'], 500);
+        }  catch (JWTException $e) {
+            return response()->json(['error' => '토큰 생성 불가.'], 500);
         }
+    
+        //사용자 이름을 토큰에 추가
+        $user = JWTAuth::user(); //현재 사용자 인스턴스 가져오기
         
+        return response()->json([
+            'token' => $token,
+            'username' => $user->username,
+            'userId' => $user->id
+        ]);
     }
+
+    
 
 }
